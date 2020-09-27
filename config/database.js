a0d48f07a360c80aedfd11aeefb9b7b0152372ae
newwebash <newtona2011@gmail.com>
2020-09-26 20:00:40 -0400

    Files created and modified with Philippa

---
+++ config/database.js
@@ -0,0 +1,19 @@
+// config/database.js
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const connectDB = async() => {
+  try {
+		const conn = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+		});
+	       console.info("Mongo is now connected to", process.env.MONGO_URI);
+	} catch (error) {
+		 console.error(error);
+		process.exit(1);
+	}
+}
+
+module.exports = connectDB;
